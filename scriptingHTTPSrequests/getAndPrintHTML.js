0372ae8291547406ff62ce4835d2b944733481ad
Your name <jaocoelho93@gmail.com>
2016-11-07 16:45:41 +0000

    added function that gets and prins entire HTML body

---
+++ scriptingHTTPSrequests/getAndPrintHTML.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+
+  https.get(requestOptions, function (response) {
+    var appended = "";
+    response.setEncoding('utf8');
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+    appended += data;
+    });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+    response.on('end', function() {
+    console.log("Here is the appended data:s \n ", appended);
+    console.log('Response stream complete.');
+  });
+
+});
+  /* Add your code here */
+
+}
+
+getAndPrintHTML();
